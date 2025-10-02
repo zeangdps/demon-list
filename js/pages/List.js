ffe0ef3e08b8eb6aef507c32bd12f90e73b81f4b
Zeanaye <89664611+Zeanaye@users.noreply.github.com>
2025-10-02 12:35:56 -0400

    hope to god this works

--- js/pages/List.js
+++ js/pages/List.js
@@ -51,8 +51,8 @@ export default {
                             <p>{{ level.id }}</p>
                         </li>
                         <li>
-                            <div class="type-title-sm">Password</div>
-                            <p>{{ level.password || 'Free to Copy' }}</p>
+                            <div class="type-title-sm">Difficulty</div>
+                            <p>{{ level.difficulty || 'Demon' }}</p>
                         </li>
                     </ul>
                     <h2>Records</h2>
